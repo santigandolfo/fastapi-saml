81b159f2a929ed8ce5371b1f0f476df225e23358
Santiago Gandolfo <santigandolfo@users.noreply.github.com>
2025-01-09 15:31:24 -0300

    Improved project setup readability (#8)

--- local_starter.sh
+++ local_starter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-uvicorn src.main:app --ssl-keyfile=key.pem --ssl-certfile=cert.pem --host=0.0.0.0 --port=8000 --reload
+uv run uvicorn src.main:app --ssl-keyfile=key.pem --ssl-certfile=cert.pem --host=0.0.0.0 --port=8000 --reload
